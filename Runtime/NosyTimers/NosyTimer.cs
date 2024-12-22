879ae3458bda06a33da4745b5f5cb8627f841af4
Isaac Augusto <isaac.augusto@exitgames.com>
2024-12-22 20:27:26 -0300

    Changes to time utility

---
+++ Runtime/NosyTimers/NosyTimer.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NosyCore.NosyTimers
+{
+    
+    public class NosyTimer
+    {
+        private struct TimerEvent
+        {
+            public float Time;
+            public Action Event;
+            public bool Fired;
+            public bool Repeat;
+        }
+
+        public readonly bool Repeat;
+        public bool Running;
+        public float Progress => (float)_timeElapsed / _duration;
+        public float ElapsedTime => _timeElapsed;
+        public bool Ended => _timeElapsed >= _duration;
+
+
+        private List<TimerEvent> _timerEvents;
+        private int _duration;
+        private int _timeElapsed;
+
+        private readonly Action OnTimerStart;
+        private readonly Action OnTimerEnd;
+        
+        public NosyTimer(int duration, bool repeat = false, Action onTimerStart = default, Action onTimerEnd = default)
+        {
+            Init(duration);
+            OnTimerStart = onTimerStart;
+            OnTimerEnd = onTimerEnd;
+            _timerEvents = new List<TimerEvent>();
+            Repeat = repeat;
+        }
+        
+        private void Init(int duration)
+        {
+            // Check if duration is valid
+            if (duration <= 0)
+            {
+                throw new ArgumentException("Duration must be greater than 0 ms.");
+            }
+            
+            _duration = duration;
+            _timeElapsed = 0;
+            Running = false;
+            TimeManager.RegisterTimer(this);
+        }
+        
+        public void Start()
+        {
+            if (Running) return;
+            
+            Running = true;
+            OnTimerStart?.Invoke();
+        }
+        
+        public void Stop()
+        {
+            Running = false;
+        }
+
+        public void Restart(int duration = -1)
+        {
+            TimeManager.UnregisterTimer(this);
+            SetTimerEventsUnfired();
+            _timeElapsed = 0;
+            Init(duration > 0 ? duration : _duration);
+            Start();
+        }
+        
+        public void Update(int deltaTimeMs)
+        {
+            if (Running == false) return;
+            
+            _timeElapsed += deltaTimeMs;
+            
+            UpdateTimerEvents();
+            
+            if (_timeElapsed >= _duration)
+            {
+                Running = false;
+                OnTimerEnd?.Invoke();
+            }
+        }
+        
+        public void AddTimerEvent(float time, Action action, bool repeat = false)
+        {
+            if (time <= 0 || time >= _duration)
+            {
+                throw new ArgumentException($"Invalid time value [{time}]. Should be bigger than 0 ms and less than the duration of the timer [{_duration}].");
+            }
+            
+            _timerEvents.Add(new TimerEvent
+            {
+                Time = time,
+                Event = action,
+                Fired = false,
+                Repeat = repeat
+            });
+        }
+        
+        private void UpdateTimerEvents()
+        {
+            if (Running == false) return;
+            
+            for (int i = _timerEvents.Count - 1; i >= 0; i--)
+            {
+                var timerEvent = _timerEvents[i];
+                if (timerEvent.Fired) continue;
+                
+                if (_timeElapsed >= timerEvent.Time)
+                {
+                    timerEvent.Fired = true;
+                    timerEvent.Event?.Invoke();
+                    if (timerEvent.Repeat == false)
+                    {
+                        _timerEvents.RemoveAt(i);
+                        continue;
+                    }
+                }
+
+                _timerEvents[i] = timerEvent;
+            }
+        }
+        
+        private void SetTimerEventsUnfired()
+        {
+            for (var index = 0; index < _timerEvents.Count; index++)
+            {
+                var timerEvent = _timerEvents[index];
+                timerEvent.Fired = false;
+                _timerEvents[index] = timerEvent;
+            }
+        }
+    }
+}
---
+++ Runtime/NosyTimers/TimeManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.LowLevel;
+using UnityEngine.PlayerLoop;
+
+
+namespace NosyCore.NosyTimers
+{
+    public static class TimeManager
+    {
+        public static bool UseUnscaledTime { get; set; } = false;
+        public static bool Running { get; private set; }
+
+        private static LinkedList<NosyTimer> _activeTimers;
+        
+        public static void Pause() => Running = false;
+        public static void Resume() => Running = true;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void Init()
+        {
+            _activeTimers = new LinkedList<NosyTimer>();
+            
+            var timerManagerLoop = new TimerManagerUpdateLoop();
+            UnityLoop.UnityLoopUtils.RegisterToUpdate(timerManagerLoop, before: typeof(Update));
+            Running = true;
+        }
+
+#if UNITY_EDITOR
+        [UnityEditor.InitializeOnLoadMethod]
+        private static void ResetStaticMembers()
+        {
+            UnityEditor.EditorApplication.playModeStateChanged += state =>
+            {
+                if (state == UnityEditor.PlayModeStateChange.ExitingPlayMode)
+                {
+                    _activeTimers.Clear();
+                    
+                    var defaultLoop = PlayerLoop.GetDefaultPlayerLoop();
+                    PlayerLoop.SetPlayerLoop(defaultLoop);
+                }
+            };
+        }
+#endif
+
+        private static void TickTimers()
+        {
+            if (!Running) return;
+            
+            int deltaTimeMs = (int)((UseUnscaledTime ? Time.unscaledDeltaTime : Time.deltaTime) * 1000);
+            var item = _activeTimers.First;
+            while (item != null)
+            {
+                var next = item.Next;
+                var timer = item.Value;
+                
+                timer.Update(deltaTimeMs);
+                
+                if (timer.Running == false && timer.Ended)
+                {
+                    if (timer.Repeat)
+                    {
+                        timer.Restart();
+                    }
+                    else
+                    {
+                        UnregisterTimer(timer);
+                    }
+                }
+
+                item = next;
+            }
+        }
+
+        public static void RegisterTimer(NosyTimer nosyTimer)
+        {
+            _activeTimers?.AddLast(nosyTimer);
+        }
+
+        public static void UnregisterTimer(NosyTimer nosyTimer)
+        {
+            _activeTimers?.Remove(nosyTimer);
+        }
+        
+        private struct TimerManagerUpdateLoop : UnityLoop.UnityLoopUtils.ICustomPlayerLoop
+        {
+            public PlayerLoopSystem.UpdateFunction UpdateFunction => TickTimers;
+        }
+    }
+}
