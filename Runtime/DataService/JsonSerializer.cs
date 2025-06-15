6b981a07d38a0fe34b942a659d5f9154a3cc6051
Isaac <isaac.augusto@hotmail.com>
2025-06-15 18:48:44 -0300

    Dev: testing

--- Runtime/DataService/JsonSerializer.cs
+++ Runtime/DataService/JsonSerializer.cs
@@ -4,12 +4,12 @@ namespace NosyCore.DataService
 {
     public class JsonSerializer : ISerializer
     {
-        public string Serialize<T>(T obj)
+        public string Serialize<T>(T obj) where T : ISerializable
         {
             return JsonUtility.ToJson(obj, true);
         }
 
-        public T Deserialize<T>(string data)
+        public T Deserialize<T>(string data) where T : ISerializable
         {
             return JsonUtility.FromJson<T>(data);
         }
